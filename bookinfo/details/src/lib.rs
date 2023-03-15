e01b2b2ca2180012ce41019901e864ea2e15aba0
knuesting.till.ext@siemens.com <tillknuesting@gmail.com>
2023-03-15 17:40:58 +0000

    return book json

--- bookinfo/details/src/lib.rs
+++ bookinfo/details/src/lib.rs
@@ -3,13 +3,27 @@ use spin_sdk::{
     http::{Request, Response},
     http_component,
 };
+use serde::Serialize;
 
-/// A simple Spin HTTP component.
+#[derive(Serialize)]
+struct Book {
+    id: u32,
+    title: String,
+    author: String,
+}
+
+
 #[http_component]
 fn handle_details(req: Request) -> Result<Response> {
-    println!("{:?}", req.headers());
+    let book =  Book {
+        id: 1,
+        title: "Sample Book".to_string(),
+        author: "John Doe".to_string(),
+    };
+    let book_json = serde_json::to_string(&book)?;
+
     Ok(http::Response::builder()
         .status(200)
-        .header("foo", "bar")
-        .body(Some("Hello, Fermyon".into()))?)
+        .header("Content-Type", "application/json")
+        .body(Some(book_json.into()))?)
 }
