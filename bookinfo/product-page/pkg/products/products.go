4a789f9b4dd22f1d3a3c7e853062e8998e5b6e5f
James Milligan <james@omnant.co.uk>
2023-03-30 16:20:35 +0100

    cleanup
    Signed-off-by: James Milligan <james@omnant.co.uk>

--- bookinfo/product-page/pkg/products/products.go
+++ bookinfo/product-page/pkg/products/products.go
@@ -11,7 +11,7 @@ func NewProductHandler() *ProductHandler {
 			{
 				ID:              0,
 				Title:           "The Comedy of Errors",
-				DescriptionHtml: `<a href="https://en.wikipedia.org/wiki/The_Comedy_of_Errors">Wikipedia Summary</a>: The Comedy of Errors is one of <b>William Shakespeare\'s</b> early plays. It is his shortest and one of his most farcical comedies, with a major part of the humour coming from slapstick and mistaken identity, in addition to puns and word play.`,
+				DescriptionHtml: `<a href="https://en.wikipedia.org/wiki/The_Comedy_of_Errors">Wikipedia Summary</a>: The Comedy of Errors is one of <b>William Shakespeare's</b> early plays. It is his shortest and one of his most farcical comedies, with a major part of the humour coming from slapstick and mistaken identity, in addition to puns and word play.`,
 			},
 		},
 	}
@@ -25,19 +25,6 @@ type Product struct {
 	ID              int    `json:"id"`
 	Title           string `json:"title"`
 	DescriptionHtml string `json:"descriptionHtml"`
-}
-
-func (p *ProductHandler) GetProducts() Products {
-	return p.products
-}
-
-func (p *ProductHandler) GetProduct(id int) *Product {
-	for _, product := range p.products {
-		if product.ID == id {
-			return &product
-		}
-	}
-	return nil
 }
 
 //tinyjson:json
@@ -78,4 +65,17 @@ type Rating struct {
 type ProductRatings struct {
 	ID      int            `json:"id"`
 	Ratings map[string]int `json:"ratings"`
+}
+
+func (p *ProductHandler) GetProducts() Products {
+	return p.products
+}
+
+func (p *ProductHandler) GetProduct(id int) *Product {
+	for _, product := range p.products {
+		if product.ID == id {
+			return &product
+		}
+	}
+	return nil
 }
